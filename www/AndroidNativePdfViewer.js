ed25d40ef6d53ce307e61aab87ee92ed06bff2ab
neinhart <neinhart@sk.com>
2017-04-05 22:26:27 +0900

    1. Feature implemented that open PDF from URI 2. Plugin pass result PDF page number

--- www/AndroidNativePdfViewer.js
+++ www/AndroidNativePdfViewer.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.openPdfUrl = function(url, header,options,success, error) {
     exec(success, error, "AndroidNativePdfViewer", "openPdfUrl", [url,header,options]);
 };
+
+exports.openPdfUri = function(uri, options,success, error) {
+    exec(success, error, "AndroidNativePdfViewer", "openPdfUri", [uri,options]);
+};
